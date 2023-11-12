8c5b22492231b3872bd216ea58b79f5bcf95fb56
Bryan Hansen <bryankhansen@gmail.com>
2023-11-12 15:58:32 -0700

    Initial commit

---
+++ src/main/java/com/barclays/BHSMessaging/BhsMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.BHSMessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhsMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhsMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barclays/BHSMessaging/BhsMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclays.BHSMessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhsMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
